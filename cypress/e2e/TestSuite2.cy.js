8363e50da43b9d4be9478a6743d41845ea7e7cde
Yeow Ji Mi <jimi@99.co>
2023-12-13 13:39:58 +0800

    Updated repo structure

---
+++ cypress/e2e/TestSuite2.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+/* 
+  Welcome QA candidate! Good luck in writing your test script! 
+  You may structure or name your file however you see fit as this is just a template
+*/
+
+describe("Test Suite 2", () => {
+  beforeEach(() => {
+    //Optional
+  });
+
+  it("Test Case 1", () => {
+    //Write your automation script here for Test Case 1
+  });
+
+  it("Test Case 2", () => {
+    //Optional
+  });
+
+  it("Test Case 3", () => {
+    //Optional
+  });
+});
